db7446b7f8260fba32dadbd45e0c2389112bdb5c
erfijodetotti <124669147+erfijodetotti@users.noreply.github.com>
2023-02-07 11:57:48 +0100

    Add files via upload

---
+++ p.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    
+     
+
+    printf("inserisci un numero a\n");
+    scanf("%d", &a);
+    printf("inserisci un numero b\n"); 
+    scanf("%d", &b);
+    int risultato = a / b;
+    printf("divisione di a e b: %d\n", risultato);
+    
+    
+}
