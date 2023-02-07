5cdb9d662afc08176caf4ee7d1cb7a9b81e6c804
erfijodetotti <124669147+erfijodetotti@users.noreply.github.com>
2023-02-07 10:27:19 +0100

    Add files via upload

---
+++ o.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int main()
+ {
+    char x = 'a';
+    char y = 'b';
+ 
+ printf("x era: %c\n", x);
+ printf("y era: %c\n", y);
+
+
+                     
+
+
+
+                
+            
+        
+
+    
+ }
